d68b681fd9c4f6120714c5eee2f7b523356da4a1
farha <jawed.f@husky.neu.edu>
2018-11-20 15:33:45 -0500

    boilerplate to get started

---
+++ src/containers/FoodieWeb.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "../components/Home/Home";
+
+
+const FoodieWeb = () =>
+    <Router>
+        <div>
+            <Route exact path="/" component={Home} />
+        </div>
+    </Router>
+
+export default FoodieWeb;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import 'jquery';
+import 'popper.js';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import './index.css';
+import FoodieWeb from './containers/FoodieWeb';
+
+
+
+ReactDOM.render(<FoodieWeb/>, document.getElementById('root'));
+
