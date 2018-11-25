936c4985def57805588a6f9a5160516cce21bece
farha <jawed.f@husky.neu.edu>
2018-11-25 04:15:25 -0500

    Took header out of container

--- src/components/DishDetails/DishDetails.js
+++ src/components/DishDetails/DishDetails.js
@@ -30,42 +30,44 @@ export default class DishDetails extends Component{
     render(){
         console.log(this.state.dish)
         return(
-            <div className="container-fluid">
+            <div>
                 <Header className="ml-0 mr-0 pl-0 pr-0"/>
-                {this.state.dish.length!==0?
-                    <div>
-                        <h3 className="text-center p-4">{this.state.dish.name}</h3>
-                <div className="row m-2 ">
-                    <div className="col-md-4 m-0">
-                    <div className="img-fluid">
-                        <div><a><img src={this.state.dish.images[0].hostedLargeUrl}/></a></div>
-                        <div className="h5 text-info p-4">Time to prepare: {this.state.dish.prepTime}</div>
-                        <div className="pills mb-3"> {"#"+ this.state.dish.attributes.course} </div>
+                <div className="container-fluid">
+                    {this.state.dish.length!==0?
+                        <div>
+                            <h3 className="text-center p-4">{this.state.dish.name}</h3>
+                    <div className="row m-2 ">
+                        <div className="col-md-4 m-0">
+                        <div className="img-fluid">
+                            <div><a><img src={this.state.dish.images[0].hostedLargeUrl}/></a></div>
+                            <div className="h5 text-info p-4">Time to prepare: {this.state.dish.prepTime}</div>
+                            <div className="pills mb-3"> {"#"+ this.state.dish.attributes.course} </div>
 
-                        <ul className="p-0">
-                            <span className="h2">Rating </span>
-                            {this.createElements(this.state.dish.rating)}
-                        </ul>
-                    </div>
-                    </div>
-                    <div className="col-md-8 img-thumbnail">
-                        <h2 className="ml-2 mb-4">Ingredients</h2>
-                        <ul className="list-group">
-                            {
-                                this.state.dish.ingredientLines.map((line, index) =>
-                                    <li className="list-group-item list-group-item-info">
-                                        {line}
-                                    </li>
-                                )
-                            }
-                        </ul>
-                        <div className="text-center mt-3">
-                            <a className="btn btn-success text-light" href={this.state.dish.source.sourceRecipeUrl} target="_blank">View Recipe</a>
+                            <ul className="p-0">
+                                <span className="h2">Rating </span>
+                                {this.createElements(this.state.dish.rating)}
+                            </ul>
                         </div>
-                    </div>
+                        </div>
+                        <div className="col-md-8 img-thumbnail">
+                            <h2 className="ml-2 mb-4">Ingredients</h2>
+                            <ul className="list-group">
+                                {
+                                    this.state.dish.ingredientLines.map((line, index) =>
+                                        <li className="list-group-item list-group-item-info">
+                                            {line}
+                                        </li>
+                                    )
+                                }
+                            </ul>
+                            <div className="text-center mt-3">
+                                <a className="btn btn-success text-light" href={this.state.dish.source.sourceRecipeUrl} target="_blank">View Recipe</a>
+                            </div>
+                        </div>
 
+                    </div>
+                        </div>:""}
                 </div>
-                    </div>:""}
             </div>
         )
     }
