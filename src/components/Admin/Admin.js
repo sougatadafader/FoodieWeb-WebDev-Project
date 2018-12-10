160133604821a3e0391773426cd646a5dc37e222
sougatadafader <sougatadafader@gmail.com>
2018-12-09 21:24:18 -0500

    admin panel started

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -9,7 +9,8 @@ export default class Admin extends Component {
         super(props)
 
         this.state = {
-            users: []
+            users: [],
+            selectedUser:{}
         }
     }
     componentDidMount=()=>
@@ -20,26 +21,42 @@ export default class Admin extends Component {
             })
         )}
 
+    selectUser = user => {
+        this.setState({
+            selectedUser: user,
+        })
+    }
+
+
     render() {
         return (
             <div className="container-fluid ml-0 mr-0 pl-0 pr-0">
                 <Header/>
             <table className="table">
+
                 <thead>
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Username</th>
                     <th scope="col">Email</th>
                     <th scope="col">Password</th>
+                    <th scope="col">Role</th>
                     <th scope="col"></th>
                 </tr>
                 </thead>
                 <tbody>
-
+                <tr>
+                    <th><label></label></th>
+                    <td><input type="text" className="col-sm-5" id="fldUsername"/></td>
+                    <td><input type="text" className="col-sm-5" id="fldEmail"/></td>
+                    <td><input type="text" className="col-sm-5" id="fldPassword"/></td>
+                    <td><input type="text" className="col-sm-5" id="fldPassword"/></td>
+                    <td><button className=" btn btn-md btn-primary pl-lg-1 mb-1" id="addUpdateBtn">Add</button></td>
+                </tr>
                 {this.state.users.length!==0?
                     this.state.users.map((user,index)=>
 
-                        (<UserRow user={user} key={index}/>) ): "" }
+                        (<UserRow user={user} key={index} selectedUser={this.state.selectedUser} selectUser={this.selectUser}/>) ): "" }
                 </tbody>
             </table>
             </div>
--- src/components/Admin/UserRow/UserRow.js
+++ src/components/Admin/UserRow/UserRow.js
@@ -1,4 +1,27 @@
-import {Component} from "react";
+import React from 'react'
+
+const Admin = ({user, deleteUser, selectUser, selectedUser, editUser}) =>
+
+    <tr className={(selectedUser===user) ? 'bg-primary text-light': ''} id={user.id} onClick={() => selectUser(user)}>
+        {console.log(user)}
+        <th scope="row" >{user.id}</th>
+        <td>{user.username}</td>
+        <td>{user.email}</td>
+        <td>{user.password}</td>
+        <td>{user.userRole}</td>
+        <td>
+        <button onClick={() => editUser(user)} className="btn fa fa-pencil ">
+        </button>
+        <button onClick={() => deleteUser(user)} className="btn fa fa-trash  ml-2">
+        </button>
+        </td>
+    </tr>
+
+
+export default Admin
+
+
+{/*import {Component} from "react";
 import React from "react";
 
 export default class Admin extends Component {
@@ -6,15 +29,25 @@ export default class Admin extends Component {
         super(props)
 
         this.state = {
-            selectedId:''
+            selectedUser : this.props.selectedUser,
+            selectedId:'',
+            isSelected:false,
+            isEdit:''
         }
 
+    }
+    selectRow=event =>{
+        let id = event.target.value;
+        this.setState({
+            selectedUser: this.props.user,
+            isSelected:true
+        })
     }
 
     render() {
         return(
-            <tr >
-                <th scope="row" onClick={this.state.selectedId = this.props.key}>{this.props.user.id}</th>
+            <tr onClick={this.selectRow} className={this.state.isSelected ? ' bg-warning': ''} value={this.props.user.id}>
+                <th scope="row" >{this.props.user.id}</th>
                 <td>{this.props.user.username}</td>
                 <td>{this.props.user.email}</td>
                 <td>{this.props.user.password}</td>
@@ -27,4 +60,4 @@ export default class Admin extends Component {
 
         )
     }
-}
+}*/}
